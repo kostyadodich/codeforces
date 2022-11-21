1738b000275952c6d0f0047181716c2ea61866f6
Kostya Dodich <dodich@mail.ru>
2022-11-22 02:21:21 +0300

    task2

--- leatcode/task2/main.go
+++ leatcode/task2/main.go
@@ -1,1 +1,28 @@
-package task2
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 1, 2, 2, 3}
+	fmt.Println(removeDuplicates(nums))
+
+}
+
+func removeDuplicates(nums []int) int {
+	index := 0
+	for i := 0; i < len(nums); i++ {
+		if len(nums) == 0 || i == len(nums)-1 {
+			if nums[len(nums)-1] != nums[len(nums)-2] {
+				nums[index] = nums[i]
+			}
+			break
+		}
+		if nums[i] == nums[i+1] {
+			continue
+		} else {
+			nums[index] = nums[i]
+		}
+	}
+
+	return index + 1
+}
