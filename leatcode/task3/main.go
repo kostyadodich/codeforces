2adca964aaaa04676b599d18f758f188cdcc08f5
Kostya Dodich <dodich@mail.ru>
2022-11-24 03:51:11 +0300

    add u-test task4 and task3

--- leatcode/task3/main.go
+++ leatcode/task3/main.go
@@ -1,1 +1,23 @@
-package task3
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	fmt.Println(removeElement(nums, 2))
+
+}
+func removeElement(nums []int, val int) int {
+	index := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+			i--
+			index++
+		}
+	}
+	fmt.Println(nums)
+
+	return index
+}
